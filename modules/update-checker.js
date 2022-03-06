d7551230ebc907a73ea734382e623a7d6ff101ef
jopgamer <59136132+jopgamer@users.noreply.github.com>
2022-03-06 18:55:24 +0100

    Update checks - disable setting

--- modules/update-checker.js
+++ modules/update-checker.js
@@ -1,18 +1,21 @@
 const axios = require('axios')
 const package = require('../package.json')
+const config = require('../config')
 
 module.exports = async () => {
-    const data = await axios({
-        url: `https://jopgamer.tk/octobot/updatecheck`,
-        headers: {
-            'X-Api-Key': package.version,
-            'Content-Type': 'application/json'
+    if (config.check_for_updates) {
+        const data = await axios({
+            url: `https://jopgamer.tk/octobot/updatecheck`,
+            headers: {
+                'X-Api-Key': package.version,
+                'Content-Type': 'application/json'
+            }
+        })
+    
+        if (data.data.status == 1) {
+            console.log("")
+            console.log(`New update awalible: ${package.version} --> ${data.data.version}`)
+            console.log("")
         }
-    })
-
-    if (data.data.status == 1) {
-        console.log("")
-        console.log(`New update awalible: ${package.version} --> ${data.data.version}`)
-        console.log("")
     }
 }
